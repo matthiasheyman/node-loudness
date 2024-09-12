188abb5683406471cf96533608ed77a8b63a6705
Matthias Heyman <matthias.heyman@ebo-enterprises.com>
2024-09-12 14:30:18 +0200

    lint: fix xo linting

--- test/basics.ts
+++ test/basics.ts
@@ -1,32 +1,34 @@
-import { expect, test, beforeEach, afterEach } from "vitest";
-import * as loudness from "../index.js";
+import {
+	expect, test, beforeEach, afterEach,
+} from 'vitest';
+import * as loudness from '../index.js';
 
 let systemVolume: number;
 let isMuted: boolean;
 
 beforeEach(async () => {
-  await Promise.all([
-    loudness.getVolume().then((v) => {
-      systemVolume = v;
-    }),
-    loudness.getMuted().then((m) => {
-      isMuted = m;
-    }),
-  ]);
+	await Promise.all([
+		loudness.getVolume().then(v => {
+			systemVolume = v;
+		}),
+		loudness.getMuted().then(m => {
+			isMuted = m;
+		}),
+	]);
 });
 
 afterEach(async () => {
-  await Promise.all([loudness.setVolume(systemVolume), loudness.setMuted(isMuted)]);
+	await Promise.all([loudness.setVolume(systemVolume), loudness.setMuted(isMuted)]);
 });
 
-test("should set and get the volume", async () => {
-  await loudness.setVolume(15);
-  const vol = await loudness.getVolume();
-  expect(vol).toBe(15);
+test('should set and get the volume', async () => {
+	await loudness.setVolume(15);
+	const vol = await loudness.getVolume();
+	expect(vol).toBe(15);
 });
 
-test("should set and get the mute state", async () => {
-  await loudness.setMuted(true);
-  const mute = await loudness.getMuted();
-  expect(mute).toBe(true);
+test('should set and get the mute state', async () => {
+	await loudness.setMuted(true);
+	const mute = await loudness.getMuted();
+	expect(mute).toBe(true);
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vitest/config";
+import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    // ... Specify options here.
-    include: ["test/**/*"],
-  },
+	test: {
+		// ... Specify options here.
+		include: ['test/**/*'],
+	},
 });
